0b7e9c7a0c855626b33a4e4a670c8838149ad328
arseniy <7558414@list.ru>
2026-02-12 23:34:45 +0300

    update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  database: 'postgres',
-  user: 'messenger_user',
-  password: 'messenger_password',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT) || 5432,
+  database: process.env.DB_NAME || 'messenger',
+  user: process.env.DB_USER || 'messenger_user',
+  password: process.env.DB_PASSWORD || 'messenger_password',
 });
 
 module.exports = pool;
